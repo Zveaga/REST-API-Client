8aea994e558a05b42f3b9c92bba63622ef3dc3c4
Rares <rares.rares.cox@gmail.com>
2024-03-19 20:38:36 +0100

    class member functions added

--- CurlRequest.cpp
+++ CurlRequest.cpp
@@ -43,9 +43,9 @@ std::vector<std::string>CurlRequest::initApiUrl()
 /**
  * [describe function]
 */
-void 	CurlRequest::initWebhooks()
+void 	CurlRequest::initWebhook()
 {
-
+	
 }
 
 /**
@@ -81,4 +81,59 @@ std::string CurlRequest::getWebhookURL() const
 }
 
 
+/**
+ * This function will be used by curl to retrieve and store the requested data
+*/
+size_t CurlRequest::curlWriteCallBack(void *retrieved_content, size_t elem_size, size_t elem_count, void *data)
+{
+    ((std::string*)data)->append((char*)retrieved_content, elem_size * elem_count);
+    return (elem_size * elem_count);
+}
+
+void CurlRequest::setCurlGetOptions()
+{
+
+}
+
+void CurlRequest::setCurlPostOptions()
+{
+
+}
+
+
+bool CurlRequest::performCurlRequest(const std::vector<std::string> &api_urls, std::vector<std::string> &curl_json_responses)
+{
+	// Initialize curl handle
+	CURL *curl = curl_easy_init();
+	std::string api_key = "";
+	if (!curl)
+	{
+		std::cerr << "Failed to initialize the CURL handle\n";
+		return (false);
+	}
+
+	// Add the api_key as a custom header to HTTP request
+	struct curl_slist *headers = nullptr;
+	headers = curl_slist_append(headers, ("X-CMC_PRO_API_KEY: " + api_key).c_str());	
+	
+	// Set up curl options
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallBack);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+	// Execute the HTTP request
+	CURLcode res = curl_easy_perform(curl);
+	// Clean up
+	curl_slist_free_all(headers);
+	curl_easy_cleanup(curl);
+
+	// Check if request was successful
+	if (res != CURLE_OK)
+	{
+		std::cerr << "Failed to perform the HTTP get request: " << curl_easy_strerror(res) << std::endl;
+        return false;
+    }
+	return (true);
+}
 
--- CurlRequest.hpp
+++ CurlRequest.hpp
@@ -11,7 +11,7 @@ class CurlRequest
 {
 	private:
 		std::vector<std::string> _api_urls;
-		std::vector<std::string> curl_json_responses;
+		std::vector<std::string> _curl_json_responses;
 		std::string _api_key;
 		std::string _webhook_url;
 
@@ -24,13 +24,22 @@ class CurlRequest
 		// --Overloads-- //
 
 		// --Member Functions-- //
-		std::vector<std::string>	initApiUrl();
-		void 						initWebhooks();
-		void 						setApiKey(std::string api_key);
-		void 						setWebhookURL(std::string webhook_url);
-		std::string 				getApiKey() const;
-		std::string 				getWebhookURL() const;
+		std::vector<std::string>initApiUrl();
+		void 					initWebhook();
+		void 					setApiKey(std::string api_key);
+		void 					setWebhookURL(std::string webhook_url);
+		std::string 			getApiKey() const;
+		std::string 			getWebhookURL() const;
 
+		size_t 					curlWriteCallBack(void *retrieved_content,
+								size_t elem_size, size_t elem_count, void *data);
+		void 					setCurlGetOptions();
+		void 					setCurlGetHeaders();
+		void 					setCurlPostOptions();
+		void 					setCurlPostHeaders();
+		bool 					performCurlRequest(const std::vector<std::string> &api_urls,
+								std::vector<std::string> &curl_json_responses);
+		
 
 };
 
