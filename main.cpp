6d1195b01819c9d355601edb2babd9422dab7cee
Rares <rares.rares.cox@gmail.com>
2024-03-19 16:11:06 +0100

    basic discord webhook implemented

--- main.cpp
+++ main.cpp
@@ -45,11 +45,43 @@ bool CurlRequest(const std::string &url, std::string &response)
 	// Check if request was successful
 	if (res != CURLE_OK)
 	{
-		std::cerr << "Failed to perform the HTTP request: " << curl_easy_strerror(res) << std::endl;
+		std::cerr << "Failed to perform the HTTP get request: " << curl_easy_strerror(res) << std::endl;
         return false;
     }
 	return (true);
 }
+
+bool sendToDiscord(const std::string &data)
+{
+	CURL *curl = curl_easy_init();
+	std::string webhook_url = "";
+
+	if (!curl)
+	{
+		std::cerr << "Failed to initialize the CURL handle\n";
+		return (false);
+	}
+	struct curl_slist *headers = nullptr;
+	headers = curl_slist_append(headers, "Content-Type: application/json");
+
+
+	curl_easy_setopt(curl, CURLOPT_URL, webhook_url.c_str());
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data.c_str());
+	curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+	CURLcode res = curl_easy_perform(curl);
+	if (res != CURLE_OK)
+	{
+		std::cerr << "Failed to perform the HTTP post request: " << curl_easy_strerror(res) << std::endl;
+        return false;
+    }
+	curl_slist_free_all(headers);
+	curl_easy_cleanup(curl);
+
+	return (true);
+
+}
+
 
 /**
  * [describe function]
@@ -79,10 +111,22 @@ bool parseJson(const std::string &jsonResponse, Json::Value &parsedRoot)
 // }
 
 
+std::string convertToJson(const Json::Value &data)
+{
+	Json::StreamWriterBuilder builder;
+	
+	const std::string data_as_json_str = Json::writeString(builder, data);
+	return (data_as_json_str);
+}
+
 int main(void)
 {
 	std::string api_url = "https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC";
 	std::string response;
+
+	// std::string cryptoJson = R"({
+    //     "content": "Bitcoin price: $50,000"
+    // 	})";
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	if (CurlRequest(api_url, response))
@@ -96,13 +140,26 @@ int main(void)
 			const Json::Value coin_data = coin_data_array[0];
 			const Json::Value quote_data = coin_data["quote"];
 			const Json::Value usd_data = quote_data["USD"];
+			const Json::Value price = usd_data["price"];
 
-			for (Json::Value::const_iterator it = usd_data.begin(); it != usd_data.end(); ++it)
-			{
-    			const std::string& key = it.key().asString();
-    			const Json::Value& value = *it;
-    			std::cout << key << ": " << value << std::endl;
-			}
+			//std::string test = convertToJson(usd_data);
+			//std::cout << test << std::endl;
+
+			std::string cryptoJson = R"({
+        	"content": "Bitcoin price: $)" + price.asString() + R"("
+    		})";
+
+			sendToDiscord(cryptoJson);
+
+
+			// --PRINT KEY_VALUE PAIRS--//
+			// for (Json::Value::const_iterator it = usd_data.begin(); it != usd_data.end(); ++it)
+			// {
+    		// 	const std::string& key = it.key().asString();
+    		// 	const Json::Value& value = *it;
+    		// 	std::cout << key << ": " << value << std::endl;
+			// }
+
 
 			// for (const Json::Value &entry : usd_data)
 			// {
